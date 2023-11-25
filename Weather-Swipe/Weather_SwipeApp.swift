07e5f65c10311f2d311042aec65d51eec6c8b87f
Mario Krajacic <krajacic.mario@gmail.com>
2023-11-25 14:10:44 +0100

    Initial Commit

---
+++ Weather-Swipe/Weather_SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_SwipeApp.swift
+//  Weather-Swipe
+//
+//  Created by Mario Krajačić on 25.11.2023..
+//
+
+import SwiftUI
+
+@main
+struct Weather_SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
