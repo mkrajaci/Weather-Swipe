3419e1991a228ccab97ce27fa456aba7a8182926
Mario Krajacic <krajacic.mario@gmail.com>
2023-11-25 14:16:01 +0100

    Initial commit

---
+++ Weather-Swipe/WeatherButton.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherButton.swift
+//  Weather-Swipe
+//
+//  Created by Mario Krajačić on 25.11.2023..
+//
+
+import Foundation
---
+++ Weather-Swipe/WeatherModels.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModels.swift
+//  Weather-Swipe
+//
+//  Created by Mario Krajačić on 25.11.2023..
+//
+
+import Foundation
